545d3f413fd48e863c0cd16731dd04bef1f37496
Abu Talha <talha007abu@gmail.com>
2026-06-18 07:54:49 +0600

    day 01 learning basic in node js

--- 01-modules/index.js
+++ 01-modules/index.js
@@ -3,8 +3,8 @@ const fs = require('node:fs');
 const contents = fs.readFileSync('notes.txt', 'utf-8');
 console.log(contents);
 
-// fs.writeFileSync('copy.txt', 'hey', 'utf-8');
-// fs.appendFileSync('copy.txt', '\n\nhey', 'utf-8');
+fs.writeFileSync('copy.txt', 'hey', 'utf-8');
+fs.appendFileSync('copy.txt', '\n\nhey', 'utf-8');
 
 // fs.mkdirSync('games/xyz/a', { recursive: true });
 // fs.rmdirSync('games');
